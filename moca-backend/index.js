192235670292d04a80fdfc0d86abd99a4a3df57c
Leonardo Vargas <lvbernal@gmail.com>
2024-02-15 17:50:45 -0500

    required modules

--- moca-backend/index.js
+++ moca-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const port = 4020;
